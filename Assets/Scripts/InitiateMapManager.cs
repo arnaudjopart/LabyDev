1c879f847f9bb45497b263279b134d2fabcc5dac
Haye Gregory <gha.dev@outlook.com>
2016-07-01 17:31:44 +0200

    GHA : MapInitialise

---
+++ Assets/Scripts/InitiateMapManager.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InitiateMapManager : MonoBehaviour {
+    #region Public and Protected Members
+
+    #endregion
+
+    #region Main Methods
+    // Use this for initialization
+    void Start()
+    {       
+       foreach(  Transform item in transform )
+        {
+            m_rooms.Add( new Room(item, item.GetComponent<MeshRenderer>(), item.name ));
+        }
+        //debugInfoRoom();
+        InitiateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region Utils
+    private void debugInfoRoom() {
+        foreach( var item in m_rooms )
+        {
+            Debug.Log( item.ToString() );
+        }
+    }
+    private void InitiateMap() {
+        int tmpIdx;
+        Color tmpColor;
+
+        // List index room
+        List<int> idxToDistribute = new List<int>(m_rooms.Count);
+        for( int i = 0; i < m_rooms.Count; i++ )
+        {
+            idxToDistribute.Add( i );
+        }
+
+        // Recupererer les coinsD
+        List<int> idxCornerMap = new List<int>();
+        idxCornerMap.Add( 0 );
+        idxCornerMap.Add( m_rooms.Count - 1 );
+        idxCornerMap.Add( m_mapWidth - 1);
+        idxCornerMap.Add((m_rooms.Count  - m_mapWidth  ));
+        //Debug.Log( idxCornerMap[ 0 ] + " " + idxCornerMap[ 1 ] + " " + idxCornerMap[ 2 ] + " " + idxCornerMap[ 3 ] + " " );
+
+        // Start Room
+        tmpIdx = Random.Range( 0, idxCornerMap.Count );
+        m_rooms[ idxCornerMap[tmpIdx] ].RoomType = TypeRoom.startRoom;
+        idxToDistribute[idxCornerMap [tmpIdx] ] = -1;
+        idxCornerMap[tmpIdx ] = -1;
+
+        // Safe Room
+        for( int i = 0; i < m_nbSafeRoom; i++ )
+        {
+            do
+                tmpIdx = Random.Range( 0, idxCornerMap.Count );
+            while( idxCornerMap[ tmpIdx ] == -1 );
+            m_rooms[ idxCornerMap[ tmpIdx ] ].RoomType = TypeRoom.safeRoom;
+            idxToDistribute[idxCornerMap[ tmpIdx ]] = -1;
+            idxCornerMap[tmpIdx ] = -1;
+        }
+
+        // Danger Room
+        for( int i = 0; i < m_nbDangerRoom; i++ )
+        {
+            do
+                tmpIdx = Random.Range( 0, idxToDistribute.Count );
+            while( idxToDistribute[ tmpIdx ] == -1 );
+
+            m_rooms[ idxToDistribute[ tmpIdx ] ].RoomType = TypeRoom.dangerRoom;
+            idxToDistribute[ tmpIdx ] = -1;
+
+        }
+        //int[] idxSafeRoom = new int[m_nbSafeRoom];
+        //int[] idxDangerRoom = new int[m_nbDangerRoom];
+        // StartPlayer
+
+        for( int i = 0; i < m_rooms.Count; i++ )
+        {
+            switch( m_rooms[i].RoomType )
+            {
+                case TypeRoom.defaultRoom: tmpColor = Color.gray;
+                    break;
+                case TypeRoom.safeRoom: tmpColor = Color.green;
+                    break;
+                case TypeRoom.dangerRoom: tmpColor = Color.red;
+                    break;
+                case TypeRoom.startRoom: tmpColor = Color.white;
+                    break;
+                default: tmpColor = Color.black;
+                    break;
+            }
+            m_rooms[ i ].MeshRenderer.materials[ 0 ].SetColor( "_Color", tmpColor );
+        }
+    }
+    
+    #endregion
+
+    #region Private Members
+    private List<Room> m_rooms = new List<Room>();
+    private Transform m_startRoom;
+
+    // ---------------------------------
+    private const int m_nbSafeRoom = 2;
+    private const int m_nbDangerRoom = 10;
+    private const int m_mapWidth = 8;
+    private const int m_mapHeigt = 4;
+
+    #endregion
+
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TypeRoom {
+    defaultRoom = 0, 
+    safeRoom = 1,
+    dangerRoom = 2,
+    startRoom
+}
+public class Room {
+
+    #region Public and Protected Members
+    public string Name { get { return m_name; } set { m_name = value; } }
+    public TypeRoom RoomType { get { return m_RoomType; } set { m_RoomType = value; } }
+    public Transform Transform { get { return m_transform; } }
+    public MeshRenderer MeshRenderer { get { return m_meshRenderer; } set { m_meshRenderer = value; } }
+    #endregion
+
+    #region Main Methods
+    public Room() { }
+    public Room(Transform _transform, MeshRenderer _meshRenderer, string _name) {
+        m_transform = _transform;
+        m_meshRenderer = _meshRenderer;
+        m_name = _name;
+        m_RoomType = 0;
+    }
+
+    public override string ToString()
+    {
+        return Name  + " - Position : " + Transform.position + MeshRenderer.materials[0].color;
+    }
+    #endregion
+
+    #region Utils
+
+    #endregion
+
+    #region Private Members
+    private Transform m_transform;
+    private MeshRenderer m_meshRenderer;
+    private string m_name;
+    private TypeRoom m_RoomType;
+    #endregion
+}
