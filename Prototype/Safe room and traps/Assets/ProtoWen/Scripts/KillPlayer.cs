e65e45c09d03babba333a9e711a309f2ef675363
Shadowsun <wendyfreedom31@hotmail.com>
2016-07-01 12:02:17 +0200

    Spike Trap

---
+++ Prototype/Safe room and traps/Assets/ProtoWen/Scripts/KillPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayer : MonoBehaviour {
+
+    #region Public & Protected Members
+
+ 
+
+    #endregion
+
+    #region Main Methods
+
+    
+
+
+    #endregion
+
+
+    #region Utils
+
+    #endregion
+
+
+    #region Private Members
+
+    #endregion
+}
---
+++ Prototype/Safe room and traps/Assets/ProtoWen/Scripts/SpikeTrapMechanism.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeTrapMechanism : MonoBehaviour
+{
+
+    #region Public & Protected Members
+
+    [Range(2f,15f)]
+    public float m_spikeInTimer;
+    [Range(.5f,3f)]
+    public float m_spikeOutTimer;
+
+    [HideInInspector]
+    public GameObject m_trap;
+    [HideInInspector]
+    public GameObject m_spike;
+    // public bool m_isTrigger;
+        
+
+    #endregion
+
+    #region Main Methods
+
+    void Start()
+    {
+        m_currentTimer = 0;
+
+     
+    }
+
+    void Update()
+    {
+
+        if( m_currentTimer >= m_spikeInTimer + m_spikeOutTimer )
+        {
+            m_currentTimer = 0;
+            m_spike.SetActive( false );
+        }
+        else if(m_currentTimer >= m_spikeInTimer )
+        {
+   
+            m_spike.SetActive( true );
+        }
+        
+            m_currentTimer += Time.deltaTime;
+        
+        }
+
+
+    #endregion
+
+
+    #region Utils
+
+    #endregion
+
+
+    #region Private Members
+
+    private float m_currentTimer;
+
+    #endregion
+}
+
