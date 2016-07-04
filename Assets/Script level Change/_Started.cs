ae1af3ebb4fa910033b22524425151ebfe237f96
arnaudjopart <arnaud.jopart@gmail.com>
2016-07-04 16:20:29 +0200

    Level Manager

---
+++ Assets/Script level Change/_Started.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class _Started : MonoBehaviour
+{
+    LevelManager level;
+
+    #region Unity Function
+    void Start()
+    {
+        level = new LevelManager();
+    }
+    #endregion
+
+    #region My Function
+    public void inputStart()
+    {
+        StartCoroutine( delayInput() );
+    }
+    #endregion
+
+    #region Numertor
+    IEnumerator delayInput()
+    {
+        yield return new WaitForSeconds( level.m_ChangeScene );
+        level.StartGame();
+    }
+    #endregion
+}
---
+++ Assets/Script level Change/_restarted.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _restarted : MonoBehaviour
+{
+    LevelManager level;
+
+    #region Function Unity
+    void Start()
+    {
+        level = new LevelManager();
+    }
+
+    #endregion
+
+    #region My Function
+    public void reStart()
+    {
+        StartCoroutine( delayInputRestart() );
+    }
+    #endregion
+
+    #region Numerator
+    IEnumerator delayInputRestart()
+    {
+        yield return new WaitForSeconds( level.m_ChangeScene );
+        level.Restart();
+    }
+
+    #endregion
+
+}
