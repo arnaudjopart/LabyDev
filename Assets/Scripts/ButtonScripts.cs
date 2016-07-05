84913141de1587685bed834c5e33cd53356d03b4
GaraQuor <trystan-delhaye@laposte.net>
2016-07-05 10:42:18 +0200

    Menu Added

--- Assets/Scripts/ButtonScripts.cs
+++ Assets/Scripts/ButtonScripts.cs
@@ -8,20 +8,36 @@ public class ButtonScripts : MonoBehaviour
     public int buttonId;
     public InputField m_IpEntry;
 
-	void Start ()
+    public Animator m_viewAnim;
+    public Animator m_connectViewAnim;
+
+    void Start ()
     {
         GetComponent<Button>().onClick.AddListener( () => Clicked() );
     }
 
     void Clicked()
     {
+        if( buttonId == -1 )
+        {
+            Debug.Log( "Quit Game" );
+            Application.Quit();
+        }
+
         if ( buttonId == 0)
+        {
+            m_viewAnim.SetTrigger( "DownView" );
+            m_connectViewAnim.SetTrigger( "UpView" );
+        }
+
+
+        if (buttonId == 1)
         {
             Global.Server = true;
             SceneManager.LoadScene( 1 );
         }
 
-        if( buttonId == 1 )
+        if( buttonId == 2 )
         {
             Global.Server = false;
 
