e1614b1f4807d0d9172fe7e232db50f43a691f3d
Baptiste Houyoux <baptisteouille@hotmail.fr>
2016-07-06 11:09:02 +0200

    Baptiste added text message system

---
+++ Assets/Scripts/UICanvas.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UICanvas : MonoBehaviour {
+    //blic Text m_sms;
+    public Text m_TextMessagePrefab;
+    public GameObject m_historic;
+	// Use this for initialization
+	void Start () {
+        //m_sms = transform.GetComponentInChildren<Text>();
+        
+        
+        LoadNewSMS( "Lorem ipsums dolor..." );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LoadNewSMS(string _text)
+    {
+        Text newText = Instantiate( m_TextMessagePrefab,transform.position,Quaternion.identity ) as Text;
+        newText.transform.SetParent( m_historic.transform, false );
+        newText.text = _text;     
+        //m_sms.text = _text;
+    }
+
+}
