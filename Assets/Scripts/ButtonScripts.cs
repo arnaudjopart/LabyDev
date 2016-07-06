0160779c43943399b4b5c678a97bf09b4a340ed5
arnaudjopart <arnaud.jopart@gmail.com>
2016-07-06 16:18:52 +0200

    Button click added

--- Assets/Scripts/ButtonScripts.cs
+++ Assets/Scripts/ButtonScripts.cs
@@ -7,54 +7,90 @@ public class ButtonScripts : MonoBehaviour
 {
     public int buttonId;
     public InputField m_IpEntry;
-
+    public AudioSource  m_clickSoundSource;
+    public AudioClip m_clickSound;
     public Animator m_viewAnim;
     public Animator m_connectViewAnim;
 
-    void Start ()
+
+    void Start()
     {
         GetComponent<Button>().onClick.AddListener( () => Clicked() );
+        m_click = Instantiate( m_clickSoundSource ) as AudioSource;
+        m_click.GetComponent<AudioSource>();
     }
 
     void Clicked()
     {
         if( buttonId == -1 )
         {
+            if( m_click != null )
+            {
+                m_click.Play();
+            }
             Debug.Log( "Quit Game" );
             Application.Quit();
         }
 
-        if ( buttonId == 0)
+        if( buttonId == 0 )
         {
+            if( m_click != null )
+            {
+                m_click.Play();
+            }
             m_viewAnim.SetTrigger( "DownView" );
             m_connectViewAnim.SetTrigger( "UpView" );
         }
 
 
-        if (buttonId == 1)
+        if( buttonId == 1 )
         {
+
+            m_click.Play();
+
             Global.Server = true;
-            SceneManager.LoadScene( 1 );
+            StartCoroutine( LoadScene( 1, 2f ) );
         }
 
-        if( buttonId == 2 ) 
+        if( buttonId == 2 )
         {
+            if( m_click != null )
+            {
+                m_click.Play();
+            }
             Global.Server = false;
 
             if( m_IpEntry.text != "" )
                 Global.Ip = m_IpEntry.text;
 
-            SceneManager.LoadScene( 1 );
+            StartCoroutine( LoadScene( 1, 2f ) );
         }
 
         if( buttonId == 3 ) // Retry - implements reload a connection / retry a game
         {
-            SceneManager.LoadScene( 1 );
+            if( m_click != null )
+            {
+                m_click.Play();
+            }
+            // SceneManager.LoadScene( 0 );
+            Debug.Log( "Retry the game" );
         }
 
         if( buttonId == 4 ) // back to the menu
         {
-            SceneManager.LoadScene( 0 );
+            if( m_click != null )
+            {
+                m_click.Play();
+            }
+            StartCoroutine( LoadScene( 0, 2f ) );
         }
     }
+
+    IEnumerator LoadScene(int index, float delayTime)
+    {
+        yield return new WaitForSeconds( delayTime );
+        SceneManager.LoadScene( index );
+    }
+
+    private AudioSource m_click;
 }
