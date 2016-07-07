6c7c46f93bf6190b3f2170d0ba3b1969d984446e
arnaudjopart <arnaud.jopart@gmail.com>
2016-07-07 11:44:12 +0200

    Corrected Trap Sound with 5s delay

--- Assets/Scripts/CollisionTrapSound.cs
+++ Assets/Scripts/CollisionTrapSound.cs
@@ -4,52 +4,53 @@ using System.Collections;
 public class CollisionTrapSound : MonoBehaviour {
 
     public AudioSource m_trapSound;
-
+    public float m_timerTrap = 5f;
  
     
    
 	void OnTriggerEnter(Collider _other)
     {
+
+        elapsedTime += Time.deltaTime;
         
-        print( "Collison "+_other.name );
-        if( _other.GetComponent<WallTriggeredByPlayer>() && !isTrapped)
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<GasTriggeredByPlayer>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<GroundTriggeredByPlayer>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<TilesTriggerScript>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<PyramideTriggerScript>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<FallingPlatTriggeredByPlayer>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
-        if( _other.GetComponent<FloorTriggeredByPlayer>() && !isTrapped )
-        {
-            isTrapped = true;
-            m_trapSound.Play();
-        }
+        if( _other.GetComponent<WallTriggeredByPlayer>() && (isTrapped || elapsedTime > m_timerTrap))
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<GasTriggeredByPlayer>() && (isTrapped || elapsedTime > m_timerTrap) )
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<GroundTriggeredByPlayer>() && (isTrapped || elapsedTime > m_timerTrap) )
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<TilesTriggerScript>() && (isTrapped || elapsedTime > m_timerTrap) )
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<PyramideTriggerScript>() && (isTrapped || elapsedTime > m_timerTrap) )
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<FallingPlatTriggeredByPlayer>() && (isTrapped || elapsedTime > m_timerTrap) )
+        
+            LaunchTrapSound();
+        
+        if( _other.GetComponent<FloorTriggeredByPlayer>() && (isTrapped || elapsedTime > m_timerTrap) )
+            LaunchTrapSound();
+
+    
         
 
     }
+    private void LaunchTrapSound()
+    {
+        isTrapped = false;
+        elapsedTime = 0f;
+        m_trapSound.Play();
+    }
 
-    private bool isTrapped;
+    private float elapsedTime = 0;
+    private bool isTrapped=true;
 }
