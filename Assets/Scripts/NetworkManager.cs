aecf5e3faeeef8a09ef73da1516ff17303f304af
GaraQuor <trystan-delhaye@laposte.net>
2016-08-02 11:46:54 +0200

    Network Final Fix

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -25,153 +25,43 @@ public class NetworkManager : MonoBehaviour
     public GameManager m_gameManager;
     public InitiateMapManager m_mapManager;
 
-    void Start ()
+    void Start()
     {
-        if( !m_instance )
-        {
-            m_instance = this;
-        }
+        m_instance = this;
 
         m_msgconnection = m_ConnectionFrame.GetComponentInChildren<Text>();
 
-        if( Global.Server )
-            StartServer();
-        else
-            StartClient( Global.Ip );
+        StartClient( Global.Ip );
     }
 
-    void Update ()
+    void Update()
     {
-        if (m_IsConnected)
+        if( m_IsConnected )
         {
             try
             {
-                if (!m_showConnect)
-                {
-                    lastPing = Time.realtimeSinceStartup;
-                    m_gameManager.m_uiCanvas.LoadNewSMS( "Jack join !" );
-                    
-                    m_showConnect = true;
-                }
+                Receive();
 
                 if( m_IsServer )
                 {
                     Send( 1, GetBytes( m_FpsLocal.transform.position ) );
                     Send( 6, GetBytes( m_FpsLocal.transform.rotation ) );
                 }
-                
-                if( !m_IsServer )
+                else
                     SendObjectif( GameManager.m_player2Objective );
 
-                SendPing();
-
-                Receive();
-
-                if (Time.realtimeSinceStartup > lastPing + 2.0f)
-                    Close();
             }
-            catch
+            catch (Exception _e)
             {
-                Close();
+                close();
+                SceneManager.LoadScene( 1 );
             }
         }
-	}
+    }
     
-    #region Utils Server
-
-    private void Close()
-    {
-        m_IsConnected = false;
-        m_showConnect = false;
-
-        if( !m_IsServer )
-            SceneManager.LoadScene( 1 );
-
-        m_gameManager.m_uiCanvas.LoadNewSMS( "Jack is gone :/" );
-
-        if( m_IsServer )
-        {
-            ResetNetwork();
-            m_IsServer = true;
-
-            InitServer();
-        }
-    }
-
-    private void InitServer()
-    {
-        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 7777);
-        
-        m_socket = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
-
-        m_socket.SetSocketOption( SocketOptionLevel.Socket, SocketOptionName.DontLinger, true );
-        m_socket.SetSocketOption( SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true );
-
-        m_socket.Bind( localEndPoint );
-
-        m_socket.Listen( 100 );
-        
-        m_socket.BeginAccept( new AsyncCallback( AcceptCallback ),
-                    m_socket );
-    }
-
-    public void AcceptCallback( IAsyncResult ar )
-    {
-        Socket listener = (Socket) ar.AsyncState;
-        Socket handler = listener.EndAccept(ar);
-
-        m_socket = handler;
-        
-        m_IsConnected = true;
-    }
-
-    private void InitFpsPlayer()
-    {
-        m_msgconnection.text = "Starting Server ...";
-        StartCoroutine( "WaitAndRemoveUIConnection", 1.0F );
-    }
-    #endregion
-
-    #region Utils Client
-    private void InitClient()
-    {
-        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(m_Ip), 7777);
-
-        m_socket = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
-
-        m_msgconnection.text = "Try connect to " + m_Ip;
-
-        try
-        {
-            m_socket.Connect( localEndPoint );
-
-            if( m_socket.Connected )
-            {
-                Debug.Log( "Connected" );
-                m_IsConnected = true;
-
-                m_UIGSM.SetActive( false );
-
-                m_FpsIcon = Instantiate( m_prefabFpsIcon );
-                m_Monitor = Instantiate( m_prefabMonitor );
-
-                SendObjectif( GameManager.m_player2Objective );
-                StartCoroutine( "WaitAndRemoveUIConnection", 1.0F );
-                RequestMapInfo();
-            }
-        }
-        catch
-        {
-            Debug.Log( "Time Out !" );
-            SceneManager.LoadScene( 1 );
-        }
-    }
-    #endregion
-
-    #region Receive
     private void Receive()
     {
-        while( m_socket.Available > 1)
+        while( m_socket.Available > 0 )
         {
             if( m_lenghtPacket != -1 )
             {
@@ -180,7 +70,8 @@ public class NetworkManager : MonoBehaviour
                     byte[] data = new byte[m_lenghtPacket];
                     m_socket.Receive( data );
 
-                    ProcessPacket( data );
+                    if ( data.Length > 0)
+                        ProcessPacket( data );
 
                     m_lenghtPacket = -1;
                 }
@@ -198,18 +89,8 @@ public class NetworkManager : MonoBehaviour
         }
     }
 
-    private void ProcessPacket(byte[] _packet)
+    private void ProcessPacket( byte[] _packet )
     {
-        if (_packet[0] == 0) //Debug
-        {
-            string array = "";
-
-            foreach( byte b in _packet )
-                array += b;
-
-            Debug.Log( array );
-        }
-        else
         if( _packet[ 0 ] == 1 ) //FPS Pos
         {
             Vector3 pos = GetVector3( SubPacket( _packet, 1, _packet.Length - 1 ) );
@@ -218,14 +99,14 @@ public class NetworkManager : MonoBehaviour
             m_FpsIcon.transform.position = pos;
         }
         else
-        if ( _packet[ 0 ] == 2 ) //Sms
+        if( _packet[ 0 ] == 2 ) //Sms
         {
             string sms = Encoding.ASCII.GetString( SubPacket( _packet, 1, _packet.Length - 1 ) );
 
             m_gameManager.m_uiCanvas.LoadNewSMS( sms );
         }
         else
-        if ( _packet[ 0 ] == 3 ) //objectif state
+        if( _packet[ 0 ] == 3 ) //objectif state
         {
             GameManager.m_player2Objective = _packet[ 1 ];
             //m_gameManager.m_uiCanvas.LoadNewSMS( "objectif changed : " + GameManager.m_player2Objective );
@@ -234,10 +115,12 @@ public class NetworkManager : MonoBehaviour
         if( _packet[ 0 ] == 4 ) //Player VR is dead
         {
             GameManager.GameOver();
+            close();
         }
         else
         if( _packet[ 0 ] == 5 ) //Set Color Rooms
         {
+            m_ConnectionFrame.SetActive( false );
             m_mapManager.ApplyIconColor( _packet[ 1 ], _packet[ 2 ] );
         }
         else
@@ -252,25 +135,80 @@ public class NetworkManager : MonoBehaviour
             GameManager.m_player1Win = true;
         }
         else
-        if ( _packet[0] == 8) //Send Map info
+        if( _packet[ 0 ] == 8 ) //Send Map info
         {
+            m_gameManager.m_uiCanvas.LoadNewSMS( "Jack is Here !" );
+
             for( int i = 0; i < m_mapManager.m_rooms.Count; i++ )
                 SendTypeRoom( i, (int)m_mapManager.m_rooms[ i ].RoomType );
         }
         else
-        if( _packet[ 0 ] == 9 ) //ping
+        if( _packet[ 0 ] == 15 ) //Linked pLayer Gone
         {
-            lastPing = Time.realtimeSinceStartup;
+            if (m_IsServer)
+                m_gameManager.m_uiCanvas.LoadNewSMS( "Jack is gone :/" );
+            else
+            {
+                close();
+                SceneManager.LoadScene( 1 );
+            }
         }
     }
 
-    public float lastPing; 
-    #endregion
+    private void StartClient(string _Ip)
+    {
+        m_Ip = _Ip;
+        m_IsServer = Global.Server;
+
+        m_lenghtPacket = -1;
+
+        InitClient();
+    }
+
+    private void InitClient()
+    {
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(m_Ip), 7777);
+
+        m_socket = new Socket( AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp );
+
+        m_msgconnection.text = "Try connect to " + m_Ip;
+
+        try
+        {
+            m_socket.Connect( localEndPoint );
+
+            if( m_socket.Connected )
+            {
+                Debug.Log( "Connected" );
+                
+                if ( m_IsServer )
+                {
+                    StartCoroutine( "WaitAndRemoveUIConnection", 1.0F );
+                }
+                else
+                {
+                    m_IsConnected = true;
+                    m_msgconnection.text = "En Attente d'un Player1";
+
+                    m_UIGSM.SetActive( false );
+                    m_FpsIcon = Instantiate( m_prefabFpsIcon );
+                    m_Monitor = Instantiate( m_prefabMonitor );
+
+                    Send( 22, new byte[] { 0 } );
+                }
+            }
+        }
+        catch
+        {
+            Debug.Log( "Time Out !" );
+            SceneManager.LoadScene( 1 );
+        }
+    }
 
     #region Send
-    private void Send(byte Id, byte[] _data)
+    private void Send( byte Id, byte[] _data )
     {
-        if (m_IsConnected)
+        if( m_IsConnected )
         {
             byte[] header = new byte[2] { (byte)(_data.Length + 1), Id };
 
@@ -278,12 +216,12 @@ public class NetworkManager : MonoBehaviour
         }
     }
 
-    public void SendSms(string _sms)
+    public void SendSms( string _sms )
     {
         Send( 2, Encoding.ASCII.GetBytes( _sms ) );
     }
 
-    public void SendObjectif(int _objectif)
+    public void SendObjectif( int _objectif )
     {
         Send( 3, new byte[] { (byte)_objectif } );
     }
@@ -291,9 +229,17 @@ public class NetworkManager : MonoBehaviour
     public void SendGameOver()
     {
         Send( 4, new byte[] { 0 } );
+        close();
     }
 
-    public void SendTypeRoom(int _room, int _type)
+    public void close()
+    {
+        m_IsConnected = false;
+        m_socket.Close();
+        m_lenghtPacket = -1;
+    }
+
+    public void SendTypeRoom( int _room, int _type )
     {
         Send( 5, new byte[] { (byte)_room, (byte)_type } );
     }
@@ -307,15 +253,10 @@ public class NetworkManager : MonoBehaviour
     {
         Send( 8, new byte[] { 0 } );
     }
-
-    public void SendPing()
-    {
-        Send( 9, new byte[] { 0 } );
-    }
     #endregion
 
     #region Utils
-    private static byte[] AddPacket(byte[] _packet1, byte[] _packet2 )
+    private static byte[] AddPacket( byte[] _packet1, byte[] _packet2 )
     {
         byte[] result = new byte[_packet1.Length + _packet2.Length];
 
@@ -325,7 +266,7 @@ public class NetworkManager : MonoBehaviour
         return result;
     }
 
-    private static byte[] SubPacket(byte[] _packet, int _index, int _length)
+    private static byte[] SubPacket( byte[] _packet, int _index, int _length )
     {
         byte[] result = new byte[_length];
         Array.Copy( _packet, _index, result, 0, _length );
@@ -337,16 +278,16 @@ public class NetworkManager : MonoBehaviour
         return BitConverter.GetBytes( _value );
     }
 
-    private static float GetFloat(byte[] _value)
+    private static float GetFloat( byte[] _value )
     {
-        return System.BitConverter.ToSingle( _value, 0);
+        return System.BitConverter.ToSingle( _value, 0 );
     }
 
-    private static byte[] GetBytes(Vector3 _value)
+    private static byte[] GetBytes( Vector3 _value )
     {
         byte[] result = new byte[12];
 
-        GetBytes( _value.x).CopyTo( result, 0 );
+        GetBytes( _value.x ).CopyTo( result, 0 );
         GetBytes( _value.y ).CopyTo( result, 4 );
         GetBytes( _value.z ).CopyTo( result, 8 );
 
@@ -365,13 +306,13 @@ public class NetworkManager : MonoBehaviour
         return result;
     }
 
-    private static Vector3 GetVector3(byte[] _value)
+    private static Vector3 GetVector3( byte[] _value )
     {
         float x = GetFloat( SubPacket( _value, 0, 4 ));
         float y = GetFloat( SubPacket( _value, 4, 4 ));
         float z = GetFloat( SubPacket( _value, 8, 4 ));
 
-        return new Vector3( x, y, z);
+        return new Vector3( x, y, z );
     }
 
     private static Quaternion GetQuaternion( byte[] _value )
@@ -385,53 +326,19 @@ public class NetworkManager : MonoBehaviour
     }
     #endregion
 
-    public void StartClient( string _Ip )
-    {
-        ResetNetwork();
-        m_Ip = _Ip;
-        m_IsServer = false;
-
-        InitClient();
-    }
-
-    public void StartServer()
-    {
-        ResetNetwork();
-        m_IsServer = true;
-
-        InitServer();
-        m_gameManager.m_uiCanvas.LoadNewSMS( "Server Started" );
-        InitFpsPlayer();
-    }
-
-    private void ResetNetwork()
-    {
-        m_showConnect = false;
-        m_IsServer = true;
-        m_IsConnected = false;
-        m_Ip = "127.0.0.1";
-        m_lenghtPacket = -1;
-        GameManager.IsGameOver = false;
-    }
-    
-    public void CloseSocket()
-    {
-        if (m_socket.Connected)
-        {
-            m_socket.Shutdown( SocketShutdown.Both );
-            m_socket.Disconnect( true );
-        }
-    }
-
     private IEnumerator WaitAndRemoveUIConnection( float _waitTime )
     {
         yield return new WaitForSeconds( _waitTime );
         m_ConnectionFrame.SetActive( false );
-        
-        if (Global.Server)
+
+        if( Global.Server )
+        {
             m_FpsLocal = (GameObject)Instantiate( m_prefabFpsLocal, Global.playerSpawnPosition, Quaternion.identity );
+            m_IsConnected = true;
+            Send( 22, new byte[] { 1 } );
+        }
     }
-    
+
     private Socket m_socket;
     private int m_lenghtPacket;
     
