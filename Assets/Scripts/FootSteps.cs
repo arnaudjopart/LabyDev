e83872b4235026dadac1868d2d5a9e7fda1f9089
arnaudjopart <arnaud.jopart@gmail.com>
2016-08-05 15:07:18 +0200

    FootSteps sound added

--- Assets/Scripts/FootSteps.cs
+++ Assets/Scripts/FootSteps.cs
@@ -4,16 +4,30 @@ using System.Collections;
 public class FootSteps : MonoBehaviour {
 
     #region Public and Protected Members 
-    public AudioClip m_steps;
- 
+    //public AudioClip m_steps1;
+    //public AudioClip m_steps2;
+    //public AudioClip m_steps3;
+    //public AudioClip m_steps4;
+    //public AudioClip m_steps5;
+    //public AudioClip m_steps6;
+    //public AudioClip m_steps7;
+
     #endregion
 
     #region Main Methdos
     void Awake()
     {
         var audioSources = gameObject.GetComponents<AudioSource>();
-        m_Audio= audioSources[ 0 ];
-      
+        m_Audio1= audioSources[ 0 ];
+        m_Audio2= audioSources[ 1 ];
+        m_Audio3= audioSources[ 2 ];
+        m_Audio4= audioSources[ 3 ];
+        m_Audio5= audioSources[ 4 ];
+        m_Audio6= audioSources[ 5 ];
+        m_Audio7= audioSources[ 6 ];
+
+
+
         m_playerPos = gameObject.GetComponent<Transform>();
     }
 
@@ -28,8 +42,8 @@ public class FootSteps : MonoBehaviour {
     {
         elapsedTime += Time.deltaTime;
         pos = m_playerPos.position;
-        isGetKeyDown =(!playSteps && (Input.GetKeyDown( "up" ) || Input.GetKeyDown( "down" ) || Input.GetKeyDown( "left" ) ||Input.GetKeyDown( "right" )));
-        isKeyAllDirections=Input.GetKeyUp( "up" ) || Input.GetKeyUp( "down" ) || Input.GetKeyUp( "left" ) || Input.GetKeyUp( "right" );
+        isGetKeyDown =(!playSteps && OVRInput.Get( OVRInput.Button.DpadUp ) || OVRInput.Get( OVRInput.Button.DpadDown ) ||OVRInput.Get( OVRInput.Button.DpadLeft ) ||OVRInput.Get( OVRInput.Button.DpadRight ));
+        isKeyAllDirections=!(OVRInput.Get( OVRInput.Button.DpadUp ) || OVRInput.Get( OVRInput.Button.DpadDown ) ||OVRInput.Get( OVRInput.Button.DpadLeft ) ||OVRInput.Get( OVRInput.Button.DpadRight ));
 
         //print( isKeyAllDirections );
         //Debug.Log( "keydown"+ " " +isGetKeyDown );
@@ -37,7 +51,14 @@ public class FootSteps : MonoBehaviour {
 
         if( isKeyAllDirections )
         {
-            m_Audio.Stop();
+            m_Audio1.Stop();
+            m_Audio2.Stop();
+            m_Audio3.Stop();
+            m_Audio4.Stop();
+            m_Audio5.Stop();
+            m_Audio6.Stop();
+            m_Audio7.Stop();
+
             isKeyAllDirections=false;
         }
 
@@ -57,12 +78,18 @@ public class FootSteps : MonoBehaviour {
         {
             if( !isGetKeyDown )
             {
-                m_Audio.Stop();
+                m_Audio1.Stop();
+                m_Audio2.Stop();
+                m_Audio3.Stop();
+                m_Audio4.Stop();
+                m_Audio5.Stop();
+                m_Audio6.Stop();
+                m_Audio7.Stop();
                 elapsedTime = 0f;
             }
-            if( Input.GetKey( "up" ) || Input.GetKey( "down" ) || Input.GetKey( "left" ) || Input.GetKey( "right" ) )
-            {
-                StartCoroutine( PlayFootstep() );
+            if( OVRInput.Get( OVRInput.Button.DpadUp ) || OVRInput.Get( OVRInput.Button.DpadDown ) ||OVRInput.Get( OVRInput.Button.DpadLeft ) ||OVRInput.Get( OVRInput.Button.DpadRight ))
+             {
+            StartCoroutine( PlayFootstep() );
             }
 
 
@@ -76,21 +103,50 @@ public class FootSteps : MonoBehaviour {
     {
 
         yield return new WaitForSeconds( 0.4f );
-        if( !m_Audio.isPlaying )
-            m_Audio.Play();
+        if( !m_Audio1.isPlaying && !m_Audio2.isPlaying && !m_Audio3.isPlaying && !m_Audio4.isPlaying && !m_Audio5.isPlaying && !m_Audio6.isPlaying && !m_Audio7.isPlaying )
+        {
+
+           count = Random.Range( 1, 8 );
+            print( count );
+            if(count == 1)
+                m_Audio1.Play();
+            if( count == 2 )
+                m_Audio2.Play();
+            if( count == 3 )
+                m_Audio3.Play();
+            if( count == 4 )
+                m_Audio4.Play();
+            if( count == 5 )
+                m_Audio5.Play();
+            if( count == 6 )
+                m_Audio6.Play();
+            if( count == 7 )
+                m_Audio7.Play();
+
+
+        }
+            
         elapsedTime = 0f;
 
     }
     #endregion
 
     #region Private Members
-    private AudioSource m_Audio;
+    private AudioSource m_Audio1;
+    private AudioSource m_Audio2;
+    private AudioSource m_Audio3;
+    private AudioSource m_Audio4;
+    private AudioSource m_Audio5;
+    private AudioSource m_Audio6;
+    private AudioSource m_Audio7;
     private Vector3 pos;
     private Transform m_playerPos;
     private float elapsedTime =0f;
     private bool playSteps;
     private bool isGetKeyDown=false;
     private bool isKeyAllDirections=false;
-   
+    private int count;
+    private Random rnd = new Random();
+
     #endregion
 }
